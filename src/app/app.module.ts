aa72a3c2449f744365c370d84955382fff8de0fe
Ivo Stinghen <iasf@icomp.ufam.edu.br>
2017-12-28 03:54:54 -0400

    Add Map && Current Position Marker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { Geolocation} from '@ionic-native/geolocation';
 @NgModule({
   declarations: [
     MyApp,
@@ -22,9 +21,12 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    Geolocation,
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
+
   ]
 })
 export class AppModule {}
