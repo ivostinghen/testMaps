d27a7022550e80c72a8a64262efbe80c4d560622
Ivo Stinghen <iasf@icomp.ufam.edu.br>
2017-12-29 18:55:30 -0400

    (Commit fix)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -90,7 +90,9 @@ export class HomePage {
     var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
     var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
     this.local.horario = localISOTime;
-   cal);
+
+
+      this.locais.push(this.local);
 
 
 
